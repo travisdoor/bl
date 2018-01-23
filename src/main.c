4491187a9fa7106a76ca6a9a9e9234c4e09b727c
Martin Dorazil <mmdorazil@gmail.com>
2018-01-23 17:47:16 +0100

    sync

--- src/main.c
+++ src/main.c
@@ -10,26 +10,7 @@ int main(int argc, char *argv[])
   printf ("Parsing: %s\n", argv[1]);
 
   FILE *f = fopen(argv[1], "rb");
-  fseek(f, 0, SEEK_END);
-  long fsize = ftell(f);
-  fseek(f, 0, SEEK_SET); 
-  char *src = malloc(fsize + 1);
-  fread(src, fsize, 1, f);
   fclose(f);
-
-  data_type_e type;
-  data_u data;
-  char *iter = src;
-
-  // test
-  char out[100000];
-  out[0] = '\0';
-
-  while(parse(&iter, &type, &data)) {
-  }
-
-  printf("out: \n%s", out);
-  free(src);
 
   return 0;
 }
