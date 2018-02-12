5692888f371d57a4c70ba4fe3982237ffbb90626
Martin Dorazil <mmdorazil@gmail.com>
2018-02-12 16:23:42 +0100

    add cpp extern C declarations in public header files

---
+++ libbl/include/bl/hash_helpers.h
@@ -0,0 +1,42 @@
+//*****************************************************************************
+// bl
+//
+// File:   hash_helpers.h
+// Author: Martin Dorazil
+// Date:   12.2.18
+//
+// Copyright 2018 Martin Dorazil
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//*****************************************************************************
+
+#ifndef BL_HASH_HELPERS_H
+#define BL_HASH_HELPERS_H
+
+#include <stdint.h>
+#include <bobject/bobject.h>
+#include "bl/type.h"
+
+extern BO_EXPORT uint32_t
+bl_hash_function(const char *ident,
+                 Type **args,
+                 int argc);
+
+
+#endif //BL_HASH_HELPERS_H
