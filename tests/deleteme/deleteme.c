6bd579424658f76297d0c943cd88f3f88a150428
Martin Dorazil <mmdorazil@gmail.com>
2019-09-23 00:51:35 +0200

    sync

---
+++ tests/deleteme/deleteme.c
@@ -0,0 +1,7 @@
+#include "deleteme.h"
+#include <stdio.h>
+
+void foo(struct Foo foo)
+{
+    printf("\ni = %d, j = %d, k = %d\n", foo.i, foo.j, foo.k);
+}
---
+++ tests/deleteme/deleteme.h
@@ -0,0 +1,12 @@
+#ifndef DELETEME_H
+#define DELETEME_H
+
+#include "deleteme_global.h"
+
+struct Foo {
+    int i,j,k;
+};
+
+DELETEME_EXPORT void foo(struct Foo foo);
+
+#endif // DELETEME_H
