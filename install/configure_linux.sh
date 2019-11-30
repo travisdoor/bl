9021b51939b0078c7e7e2814c8be2347bf6c25b8
Martin Dorazil <mmdorazil@gmail.com>
2019-11-30 18:03:13 +0100

    sync

--- install/configure_linux.sh
+++ install/configure_linux.sh
@@ -76,7 +76,7 @@ fi
 #fi
 
 RT_O=$LIB_DIR/rt/blrt_x86_64_linux.o
-LINKER_OPT="-e %RT_ENTRY_POINT RT_O --hash-style=gnu --no-add-needed --build-id --eh-frame-hdr -dynamic-linker $LDLIB -lc -lm"
+LINKER_OPT="-e $RT_ENTRY_POINT $RT_O --hash-style=gnu --no-add-needed --build-id --eh-frame-hdr -dynamic-linker $LDLIB -lc -lm"
 
 rm -f $CONFIG_FILE
 mkdir -p ../etc
